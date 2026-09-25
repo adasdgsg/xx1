458d59329df3a9245a511d480601e55e99f13054
喵喵喵 <m19005082008@163.com>
2026-09-25 22:31:54 +0800

    Add files via upload

---
+++ 2222 - 副本.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+double jia(double a, double b)
+{
+return 	a + b;
+}
+double jian(double a, double b)
+{
+	return 	a - b;
+}
+double chen(double a, double b)
+{
+	return 	a * b;
+}
+double chu(double a, double b)
+{
+	if (a!=0&&b!=0)
+	{
+		return 	a + b;
+	}
+	else
+	{
+		printf("0");
+	}
+}
+double(*AAA[4])(double, double) = {jia ,jian ,chen,chu};
+
+int main()
+{
+	double A;
+	double B;
+	int n=1;
+	scanf_s("%lf %lf %d", &A, &B,&n);
+	printf("%lf\n", AAA[n - 1](A,B));
+	return 0;
+
+}
